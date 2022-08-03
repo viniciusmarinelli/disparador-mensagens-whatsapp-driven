cedfca7a96e2564b62a8676fad0b433a0b0c6289
viniciusSouza30 <viniciussmti@gmail.com>
2022-08-02 21:44:40 -0300

    atualizando link do WhatsApp

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function mandandoMensagem(elementoHtml) {
     var textoHtml = elementoHtml.firstElementChild.innerText;
     var numeroDigitado = prompt("Digite seu número");
-    var linkWhatsApp = `https://api.whatsapp.com/?phone=55${numeroDigitado}&text=${textoHtml}`;
+    var linkWhatsApp = `https://wa.me/55${numeroDigitado}?text=${textoHtml}`;
     window.open(linkWhatsApp);
 }
 
